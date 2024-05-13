0b7d2ba99bf6a1dea9bfc1592c6d188ccaf94f49
Aayush Rai <coolaayushrai@gmail.com>
2024-05-13 12:16:16 +0530

    Create methods to check vacant board space and player move validation
    checkSpaceVacancy() method loops through all the board slots and
    returns true even if one slot is "empty" else returns false. This
    is crucial for checking whether the game is over, especially in case
    of Draws.
    
    validateMove() method takes in the argument 'position' representing
    the index number of the slot that the player wants to fill. The method
    checks whether the slot is "empty" and returns boolean value.

--- script.js
+++ script.js
@@ -9,4 +9,19 @@ const Gameboard = (function() {
         }
     };
 
+    const checkSpaceVacancy = () => {
+        for (let i=0; i<boardSize; i++)
+        {
+            if (gameboard[i] == "empty")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const validateMove = (position) => {
+        return gameboard[position] == "empty" ? true : false;
+    }
+
 })();
